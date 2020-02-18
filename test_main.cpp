cf7e0fcb762deaa5add78ce7029f777b0b357c91
Hugo Da Roit <contact@hdaroit.fr>
2020-02-18 16:09:14 +0100

    add test data

--- test_main.cpp
+++ test_main.cpp
@@ -2,10 +2,27 @@
 // Created by user on 18/02/2020.
 //
 
+#include <iostream>
 #include "Model/ModelLinear.h"
 
+
+void predictAll(ModelLinear* model) {
+    for(double i = -7; i < 8; i += 1) {
+        for(double j = 0; j < 15; j += 1) {
+            double params[] = {i, j};
+            int predicted = model->predict(params);
+            std::cout << (predicted == 0 ? "0" : predicted >= 1 ? "+" : "-");
+        }
+
+        std::cout << std::endl;
+    }
+}
 
 int main()
 {
     auto modelLi = new ModelLinear(10);
+    double trainingParams[] = {-3, 9, 6, 13, -7, 2};
+    double trainingResults[] = {1, 1, -1};
+    modelLi->train(trainingParams, 3, trainingResults, 0.01, 6);
+    predictAll(modelLi);
 }
