456128821a7d1d769598d84231a764f06abbb2d7
Hugo Da Roit <contact@hdaroit.fr>
2020-02-19 21:36:19 +0100

    wip

--- Model/ModelLinear.cpp
+++ Model/ModelLinear.cpp
@@ -30,9 +30,9 @@ void ModelLinear::regress(double inputs[], int nbOfInputsPackets, double predict
     Eigen::MatrixXd Y(nbOfInputsPackets, 1);
 
     for (int i = 0, j = 0; i < nbOfInputsPackets * packetSize; i += packetSize, j++) {
-        X(j, 0) = inputs[i];
-        X(j, 1) = inputs[i + 1];
-        X(j, 2) = 1;
+        X(j, 0) = 1;
+        X(j, 1) = inputs[i];
+        X(j, 2) = inputs[i + 1];
     }
 
     for (int i = 0; i < nbOfInputsPackets; i++) {
